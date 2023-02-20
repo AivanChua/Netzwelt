3513503697011cc02c50bc51978ac01e08db546d
Aivan <aivanchua23@gmail.com>
2023-02-20 13:07:18 +0800

    Update Home.php

--- Home.php
+++ Home.php
@@ -1,12 +1,23 @@
 <?php
 session_start();
-if($_SESSION['login'] == 0){
-	header("Location: Login.php");
+if($_SESSION['login'] == 1){
+	echo "Territories <br> <br>" ;
+	echo "Here are the list of territories";
+	$ch = curl_init();
+
+	curl_setopt($ch, CURLOPT_URL, 'https://netzwelt-devtest.azurewebsites.net/Territories/All');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+
+	$result = curl_exec($ch);
+
+	curl_close($ch);
+	
 }
 else{
-echo "Territories <br> <br>" ;
-echo "Here are the list of territories";
+	header("Location: Login.php");
 }
+
 ?>
 
 </body>
