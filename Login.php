402e695e158dafdc31f3f20066838fe6cbf80a5c
Aivan <aivanchua23@gmail.com>
2023-02-20 12:24:32 +0800

    Update Login.php

--- Login.php
+++ Login.php
@@ -5,14 +5,18 @@
 <body>
 <?php
 	if(isset($_SESSION["login"])){
-		if($_SESSION["login"] != 1){	
-		echo "Invalid username or password<br><br>";
+		if($_SESSION["login"] > 1){	
+			echo "Invalid username or password<br><br>";
+			$_SESSION["login"] = 0;
+		}
+		else if($_SESSION["login"] == 1){
+			header("Location: Home.php");
 		}
 	}
 ?>
 <form action="Main.php" method="post">
 	Username<br><input type="text" name="username"><br><br>
-	Password<br><input type="password" name="password"><br><br>
+	Password<br><input type="password" name="password'"><br><br>
 	<input type="submit" value="Login">
 </form>
 
